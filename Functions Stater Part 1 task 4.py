9b2959c7cff5f365140a9ad22bb068cb65ade62e
Andrew Batty <21553@longroad.ac.uk>
2014-11-17 12:19:54 +0000

    Funcytions starter Part 1

---
+++ Functions Stater Part 1 task 4.py
@@ -0,0 +1,7 @@
+# Andrew Batty
+# 17/11/14
+# Functions Stater Part 1 task 4
+
+hours = int(input("Please enter the number of hours you have worked this week: "))
+pay_rate = float(input("Please enter your normal rate of pay per hour: "))
+overtime_pay = int(input("Please enter the amount of overtime hours you have worked: "))
