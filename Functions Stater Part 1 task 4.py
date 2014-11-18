abf0df8256afeda07cf7d2fde7771b0452d67f7f
Andrew Batty <21553@longroad.ac.uk>
2014-11-18 15:17:13 +0000

    Functions Starter Program

--- Functions Stater Part 1 task 4.py
+++ Functions Stater Part 1 task 4.py
@@ -1,7 +1,37 @@
 # Andrew Batty
-# 17/11/14
+# 18/11/14
 # Functions Stater Part 1 task 4
 
-hours = int(input("Please enter the number of hours you have worked this week: "))
-pay_rate = float(input("Please enter your normal rate of pay per hour: "))
-overtime_pay = int(input("Please enter the amount of overtime hours you have worked: "))
+#difining basic pay
+def calculate_basic_pay(hours,pay):
+    basic_pay = hours * pay
+    return basic_pay
+
+#defining overtime pay
+def calculate_overtime_pay(hours,pay):
+    over_hours = hours - 40
+    over_pay = pay * 1.5
+    overtime_pay = over_hours * over_pay
+    basic_pay = 40 * pay
+    total_pay = basic_pay + overtime_pay
+    return total_pay
+
+def calculate_total_pay(hours,pay):
+    if hours <= 40:
+        total_pay = calculate_basic_pay(hours,pay)
+    else:
+        total_pay = calculate_overtime_pay(hours,pay)
+    return total_pay
+
+def work_details():
+    hours = int(input("Please enter the amount of hours you have worked this week: "))
+    pay = float(input("Please enter your normal rate of pay: "))
+    return (hours,pay)
+
+def diplay_total_pay(total_pay):
+    print
+    
+
+#main program
+
+    
